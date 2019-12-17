143912edbb053f3dd8f6c98989a82524527fb46b
caochunlei <ccl851764651@126.com>
2019-12-17 11:49:16 +0800

    饼图优化

--- src/app/shared/components/sf-pie/sf-pie.component.ts
+++ src/app/shared/components/sf-pie/sf-pie.component.ts
@@ -19,30 +19,32 @@ export class SfPieComponent implements OnInit, AfterViewInit {
       {
         radius: ['60%', '80%'],
         center: ['50%', '50%'],
-        padAngle: 0,
+        padAngle: 0.01,
         cornerRadius: 2,
         label: {
-          show: true
+          show: true,
+          borderWidth: 1,
+          background: '#ffffff'
         },
         data: [
-          { name: 'bbb', value: 11, selected: true },
-          { name: 'aaa', value: 11 },
-          { name: 'aaa', value: 11 },
-          { name: 'aaa', value: 11 },
-          { name: 'aaa', value: 11 },
-          { name: 'aaa', value: 11 },
-          { name: 'aaa', value: 11 },
-          { name: 'aaa', value: 11 },
-          { name: 'aaa', value: 11 },
-          { name: 'aaa', value: 11 },
-          { name: 'aaa', value: 11 },
-          { name: 'aaa', value: 11 },
-          { name: 'aaa', value: 11 },
-          { name: 'aaa', value: 11 },
-          { name: 'aaa', value: 2 },
-          { name: 'aaa', value: 3 },
-          { name: 'aaa', value: 3 },
-          { name: 'aaa', value: 1 },
+          { name: 'bbbdddddddd', value: 11 },
+          { name: 'aaa1', value: 11 },
+          { name: 'aaa2', value: 11 },
+          { name: 'aaa3', value: 11 },
+          { name: 'aaa4', value: 11 },
+          { name: 'aaa5', value: 11 },
+          { name: 'aaa6', value: 11 },
+          { name: 'aaa7', value: 11 },
+          { name: 'aaa8', value: 11 },
+          { name: 'aaa9', value: 11 },
+          { name: '床前明月光，疑是地上霜', value: 11 },
+          { name: 'aaa11', value: 11 },
+          { name: 'aaa12', value: 11 },
+          { name: 'aaa13', value: 11 },
+          { name: 'aaa14', value: 2 },
+          { name: 'aaa15', value: 3 },
+          { name: 'aaa16', value: 3 },
+          { name: 'aaa17cccccccccccccccccccccccccccccc', value: 5 },
         ]
       },
       {
@@ -55,7 +57,7 @@ export class SfPieComponent implements OnInit, AfterViewInit {
           position: 'inner'
         },
         data: [
-          { name: 'aaa1', value: 11, selected: true },
+          { name: 'aaa1', value: 11 },
           { name: 'aaa2', value: 11 },
           { name: 'aaa3', value: 11 },
           { name: 'aaa4', value: 11 },
@@ -71,6 +73,8 @@ export class SfPieComponent implements OnInit, AfterViewInit {
 
   private svg: any;
 
+  private selection: any[];
+
   constructor() { }
 
   public ngOnInit() {
@@ -83,9 +87,8 @@ export class SfPieComponent implements OnInit, AfterViewInit {
     this.svgH = Number.parseFloat(wrapDom.style('height'));
 
     this.initSvg(wrapDom);
-    this.option.series.forEach((seriesItem: any) => {
-      this.drawPie(seriesItem);
-    });
+
+    this.drawPie(this.option.series);
   }
 
   private initSvg(dom: any) {
@@ -116,9 +119,12 @@ export class SfPieComponent implements OnInit, AfterViewInit {
 
   // 初始化饼图数据
   private initPieData(data: any[], pieParams: any) {
-    return d3.pie()
+
+    return d3.pie().sort(null)
       .value((d: any) => d.value)(data)
       .map((d: any, index: number) => {
+        d.width = this.getTextWidth(d.data.name, 12);
+        d.id = d.data.name + index;
         d.arc = d.startAngle + (d.endAngle - d.startAngle) / 2;
         d.my = -Math.cos(d.arc) * pieParams.centerR;
         d.mx = Math.sin(d.arc) * pieParams.centerR;
@@ -129,56 +135,125 @@ export class SfPieComponent implements OnInit, AfterViewInit {
   }
 
   // 添加鼠标事件
-  private addMouseEvent(nodes: any, arcPathEnter: (params: any) => {}, arcPath: (params: any) => {}, centerX: number, centerY: number) {
-    nodes.on('mouseenter', function (d: any) {
-      d3.select(this).select('.pie-path')
-        .transition()
-        .duration(300)
-        .attr('d', arcPathEnter(d));
-    }).on('mouseleave', function (d: any) {
-      d3.select(this).select('.pie-path')
-        .transition()
-        .duration(300)
-        .attr('d', arcPath(d));
+  private addMouseEvent(nodes: any, arcPathEnter: any, arcPath: any, centerX: number, centerY: number, isInnerLabel: boolean) {
+    nodes.on('mouseenter', function(d: any, index: number) {
+      if (!isInnerLabel) {
+        this.parentNode.appendChild(this);
+      }
+      d3.select(this).select('.pie-path').transition().duration(300).attr('d', arcPathEnter(d));
+    }).on('mouseleave', function(d: any, index: number) {
+      if (!d.data.selected && index !== nodes.nodes().length - 1) {
+        this.parentNode.insertBefore(this, this.parentNode.childNodes[index]);
+      } else if (!d.data.selected && index === nodes.nodes().length - 1) {
+        this.parentNode.insertBefore(this, this.parentNode.childNodes[index - 1]);
+      }
+
+      d3.select(this).select('.pie-path').transition().duration(300).attr('d', arcPath(d));
     }).on('click', function (d: any, index: number) {
-      d.data.selected = !d.data.selected;
+      nodes.each((d1: any) => d1.data.selected = false);
+      d.data.selected = true;
       const dx = Math.sin(d.arc) * 5;
       const dy = -Math.cos(d.arc) * 5;
-      nodes.transition().duration(300).attr('transform', `translate(${centerX},${centerY})`);
+      d3.selectAll('.pieG').transition().duration(300).attr('transform', `translate(${centerX},${centerY})`);
       d3.select(this).transition().duration(300).attr('transform', `translate(${centerX + dx},${centerY + dy})`);
+      d3.selectAll('.pie-label-g').transition().duration(300).attr('transform', `translate(${centerX},${centerY})`);
 
-      const pieLabelGnodes = d3.select(this.parentNode).selectAll('.pie-label-g');
-      pieLabelGnodes.transition().duration(300).attr('transform', `translate(${centerX},${centerY})`);
-      d3.select(pieLabelGnodes.nodes()[index])
-        .transition()
-        .duration(300)
-        .attr('transform', `translate(${centerX + dx},${centerY + dy})`);
+      if (!isInnerLabel) {
+        this.parentNode.appendChild(this);
+      } else {
+        const pieLabelGnodes = d3.select(this.parentNode).selectAll('.pie-label-g');
+        pieLabelGnodes.transition().duration(300).attr('transform', `translate(${centerX},${centerY})`);
+        d3.select(pieLabelGnodes.nodes()[index])
+          .transition()
+          .duration(300)
+          .attr('transform', `translate(${centerX + dx},${centerY + dy})`);
+      }
     });
   }
 
+  private drawOuterLable(pieNodes: any, pieParams: any, seriesItem: any, colors: string[]) {
+    const borderRadius = (seriesItem.label && seriesItem.label.borderRadius) || 3;
+    const height = (seriesItem.label && seriesItem.label.height) || 24;
+    const width = seriesItem.label && seriesItem.label.width;
+    const isShow = seriesItem.label && seriesItem.label.show;
+    const borderWidth = (seriesItem.label && seriesItem.label.borderWidth) || 1;
+    const background = (seriesItem.label && seriesItem.label.background) || '#ffffff';
+
+    pieNodes.append('path')
+      .attr('d', (d: any) => {
+        const borderCenterX = Math.sin(d.arc) * pieParams.endR;
+        const borderCenterY = -Math.cos(d.arc) * pieParams.endR;
+        const disR = 20;
+        const disX = Math.sin(d.arc) > 0 ? 1 : -1;
+        return `
+              M${borderCenterX},${borderCenterY}
+              L${borderCenterX + disR * Math.sin(d.arc)},${borderCenterY - disR * Math.cos(d.arc)}
+              L${borderCenterX + disR * Math.sin(d.arc) + disX * 20},${borderCenterY - disR * Math.cos(d.arc)}
+               `;
+      })
+      .attr('stroke', '#999').attr('stroke-width', 0.5)
+      .attr('fill', 'none');
+
+    pieNodes.append('rect')
+      .attr('class', 'pie-label-rect')
+      .attr('rx', borderRadius)
+      .attr('ry', borderRadius)
+      .attr('width', (d: any) => width || d.width)
+      .attr('height', height)
+      .attr('stroke', (d: any, index: number) => !isShow ? 'none' : colors[index])
+      .attr('stroke-width', borderWidth)
+      .attr('fill', background)
+      .attr('transform', (d: any) => {
+        const rectW = width || d.width;
+        const borderCenterX = Math.sin(d.arc) * pieParams.endR;
+        const borderCenterY = -Math.cos(d.arc) * pieParams.endR;
+        const disR = 20;
+        const disX = Math.sin(d.arc) > 0 ? 20 : - rectW - 20;
+        return `translate(${borderCenterX + disR * Math.sin(d.arc) + disX},${borderCenterY - disR * Math.cos(d.arc) - 12})`;
+      });
+
+    pieNodes.append('text')
+      .attr('class', 'pie-label')
+      .text((d: any) => d.data.name)
+      .attr('fill', (d: any, index: number) => !isShow ? 'none' : colors[index])
+      .attr('dominant-baseline', 'middle')
+      .attr('text-anchor', (d: any) => Math.sin(d.arc) > 0 ? 'start' : 'end')
+      .style('font-size', 12)
+      .style('pointer-events', 'none')
+      .attr('transform', (d: any) => {
+        const rectW = width || d.width;
+        const borderCenterX = Math.sin(d.arc) * pieParams.endR;
+        const borderCenterY = -Math.cos(d.arc) * pieParams.endR;
+        const disR = 20;
+        const disX = Math.sin(d.arc) > 0 ? 25 : - 25;
+        return `translate(${borderCenterX + disR * Math.sin(d.arc) + disX},${borderCenterY - disR * Math.cos(d.arc)})`;
+      });
+  }
+
   // 画饼图
-  private drawPiePath(
-    wrapNode: any,
-    pieData: any[],
-    arcPathEnter: (params: any) => {},
-    arcPath: (params: any) => {},
-    pieParams: any,
-    colors: string[]
-  ) {
+  private drawPiePath(wrapNode: any, pieData: any[], seriesItem: any, pieParams: any, colors: string[]) {
+    const isInnerLabel = seriesItem.label && seriesItem.label.position === 'inner';
     const centerX = pieParams.centerX;
     const centerY = pieParams.centerY;
+    const arcPath = this.initArcPath(pieParams.startR, pieParams.endR, seriesItem.padAngle, seriesItem.cornerRadius);
+    const arcPathEnter = this.initArcPath(pieParams.startR, pieParams.endR + 8, seriesItem.padAngle, seriesItem.cornerRadius);
     const pieNodes = wrapNode.selectAll('.pieG')
       .data(pieData)
       .enter()
       .append('g')
       .attr('class', 'pieG')
+      .attr('id', (d: any) => 'pieG' + d.id)
       .attr('transform', (d: any) => {
         const dx = Math.sin(d.arc) * 5;
         const dy = -Math.cos(d.arc) * 5;
         return `translate(${centerX + (d.data.selected ? dx : 0)},${centerY + (d.data.selected ? dy : 0)})`;
       });
 
-    this.addMouseEvent(pieNodes, arcPathEnter, arcPath, centerX, centerY);
+    this.addMouseEvent(pieNodes, arcPathEnter, arcPath, centerX, centerY, isInnerLabel);
+
+    if (!isInnerLabel) {
+      this.drawOuterLable(pieNodes, pieParams, seriesItem, colors);
+    }
 
     pieNodes.append('path')
       .attr('class', 'pie-path')
@@ -190,7 +265,7 @@ export class SfPieComponent implements OnInit, AfterViewInit {
   }
 
   // 画label
-  private drawLabel(wrapNode: any, pieData: any[], pieParams: any, seriesItem: any, colors: string[]) {
+  private drawInnerLabel(wrapNode: any, pieData: any[], pieParams: any, seriesItem: any, colors: string[]) {
     const centerX = pieParams.centerX;
     const centerY = pieParams.centerY;
     const textNodes = wrapNode.selectAll('.pie-label-g')
@@ -198,67 +273,45 @@ export class SfPieComponent implements OnInit, AfterViewInit {
       .enter()
       .append('g')
       .attr('class', 'pie-label-g')
+      .attr('id', (d: any) => 'pieLabel' + d.id)
       .attr('transform', (d: any) => {
         const dx = Math.sin(d.arc) * 5;
         const dy = -Math.cos(d.arc) * 5;
         return `translate(${centerX + (d.data.selected ? dx : 0)},${centerY + (d.data.selected ? dy : 0)})`;
       });
 
-    if (seriesItem.label.position === 'inner') {
-      textNodes.append('text')
-        .attr('class', 'pie-label')
-        .text((d: any) => d.data.name)
-        .attr('fill', (d: any, index: number) => seriesItem.label.show === false ? 'none' : '#fff')
-        .attr('dominant-baseline', 'middle')
-        .attr('text-anchor', 'middle')
-        .style('font-size', 12)
-        .style('pointer-events', 'none')
-        .attr('transform', (d: any) => `translate(${d.mx},${d.my})`);
-    } else {
-      textNodes.append('path')
-        .attr('d', (d: any) => {
-          const borderCenterX = Math.sin(d.arc) * pieParams.endR;
-          const borderCenterY = -Math.cos(d.arc) * pieParams.endR;
-          const disR = 20;
-          const disX = Math.sin(d.arc) > 0 ? 1 : -1;
-          return `M${borderCenterX},${borderCenterY}
-                 L${borderCenterX + disR * Math.sin(d.arc)},${borderCenterY - disR * Math.cos(d.arc)}
-                 L${borderCenterX + disR * Math.sin(d.arc) + disX * 20},${borderCenterY - disR * Math.cos(d.arc)}
-                 `;
-        })
-        .attr('stroke', '#999').attr('stroke-width', 0.5)
-        .attr('fill', 'none');
-      textNodes.append('text')
-        .attr('class', 'pie-label')
-        .text((d: any) => d.data.name)
-        .attr('fill', (d: any, index: number) => seriesItem.label.show === false ? 'none' : colors[index])
-        .attr('dominant-baseline', 'middle')
-        .attr('text-anchor', (d: any) => Math.sin(d.arc) > 0 ? 'start' : 'end')
-        .style('font-size', 12)
-        .style('pointer-events', 'none')
-        .attr('transform', (d: any) => {
-          const borderCenterX = Math.sin(d.arc) * pieParams.endR;
-          const borderCenterY = -Math.cos(d.arc) * pieParams.endR;
-          const disR = 20;
-          const disX = Math.sin(d.arc) > 0 ? 1 : -1;
-          return `translate(${borderCenterX + disR * Math.sin(d.arc) + disX * 25},${borderCenterY - disR * Math.cos(d.arc)})`;
-        });
-    }
-
-
+    textNodes.append('text')
+      .attr('class', 'pie-label')
+      .text((d: any) => d.data.name)
+      .attr('fill', (d: any, index: number) => seriesItem.label && seriesItem.label.show === false ? 'none' : '#fff')
+      .attr('dominant-baseline', 'middle')
+      .attr('text-anchor', 'middle')
+      .style('font-size', 12)
+      .style('pointer-events', 'none')
+      .attr('transform', (d: any) => `translate(${d.mx},${d.my})`);
   }
 
-  private drawPie(seriesItem: any) {
-    const colors = seriesItem.data.map((dataItem: any, index: number) => d3.interpolateViridis(index / seriesItem.data.length));
-    console.log(colors);
+  private drawPie(data: any[]) {
+    const that = this;
+    this.svg.append('g')
+      .attr('class', 'main-pies-wrap')
+      .selectAll('.pieGwrap')
+      .data(data)
+      .enter()
+      .append('g')
+      .attr('class', 'pieGwrap')
+      .each(function (d: any) {
+        const nodeG = d3.select(this);
+        const colors = that.option.color;
+        // const colors = d.data.map((dataItem: any, index: number) => d3.interpolateViridis(index / d.data.length));
+        const pieParams = that.getPieParams(d);
 
-    const pieParams = this.getPieParams(seriesItem);
-    const arcPath = this.initArcPath(pieParams.startR, pieParams.endR, seriesItem.padAngle, seriesItem.cornerRadius);
-    const arcPathEnter = this.initArcPath(pieParams.startR, pieParams.endR + 8, seriesItem.padAngle, seriesItem.cornerRadius);
-    const pieData = this.initPieData(seriesItem.data, pieParams);
-    const pieGwrap = this.svg.append('g').attr('class', 'pieGwrap');
-    this.drawPiePath(pieGwrap, pieData, arcPathEnter, arcPath, pieParams, colors);
-    this.drawLabel(pieGwrap, pieData, pieParams, seriesItem, colors);
+        const pieData = that.initPieData(d.data, pieParams);
+        that.drawPiePath(nodeG, pieData, d, pieParams, colors);
+        if (d.label.position === 'inner') {
+          that.drawInnerLabel(nodeG, pieData, pieParams, d, colors);
+        }
+      });
   }
 
   private setRatio(ratio: any) {
@@ -270,6 +323,16 @@ export class SfPieComponent implements OnInit, AfterViewInit {
     } else {
       return 0;
     }
+  }
+
+  private getTextWidth(str: string, fontSize = 14) {
+    const span = d3.select('body').append('span')
+    .style('display', 'inline-block')
+    .style('font-size', fontSize + 'px')
+    .text(str);
+    const spanWidth = Number.parseFloat(span.style('width')) + 10;
+    span.remove();
+    return spanWidth;
   }
 
 }
